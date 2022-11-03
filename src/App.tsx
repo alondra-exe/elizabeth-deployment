79d7a25503de407a84c38513e7af2ba83ce32ed0
alondra-exe <aeli_ds@hotmail.com>
2022-11-03 17:53:27 -0600

    name adjust

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Alondra Elizabeth Delgadillo Silos.</p>
+        <p>Alondra Elizabeth Delgadillo Silos</p>
         <a
           className="App-link"
           href="https://reactjs.org"
